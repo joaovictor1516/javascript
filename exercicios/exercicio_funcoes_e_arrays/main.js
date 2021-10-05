404352d2cb4f575f1f73a8cb828d2f6aaa1c7ace
João Victor Campos Souza Oliveira <voliveira.joao30@sempreceub.com>
2021-10-05 14:06:01 -0300

    adição e configuração do botão remover

--- exercicios/exercicio_funcoes_e_arrays/main.js
+++ exercicios/exercicio_funcoes_e_arrays/main.js
@@ -40,7 +40,33 @@ function adicionar(){
     mostra_resultado.focus();
 }
 
+function remover(){
+    if(confereIntervalo(input_numero.value) && confereLista(input_numero.value, lista)){
+        lista.splice(lista.indexOf(Number(input_numero.value)),1);
+        let elemento = document.createElement('option');
+        elemento.text = `${input_numero.value} Foi removido.`;
+        select_lista.appendChild(elemento);
+     } 
+     else if(input_numero.value.length === 0){
+         alert('Por favor digite um valor entre 1 e 100.');
+     } 
+     else {
+         alert('Esse número não está listado.');
+     }
+    input_numero.value = '';
+    input_numero.focus();
+    mostra_resultado.innerText = '';
+    mostra_resultado.focus();
+}
+
 function finalizar(){
-    let mostra_resultado = document.querySelector('div#mostra_resultado');
-    mostra_resultado.innerHTML = `Tamanho da lista: ${lista.length}` + `<br>Elementos da lista: ${lista}` + `<br>Maior elemento da lista: ${Math.min(...lista)}` + `<br>Menor elemento da lista: ${Math.min(...lista)}`;
+    if(select_lista.length === 0 || lista.length === 0){
+        alert('A lista está vazia.');
+    } else{
+        let mostra_resultado = document.querySelector('div#mostra_resultado');
+        mostra_resultado.innerHTML = `Tamanho da lista: ${lista.length}`;
+        mostra_resultado.innerHTML += `<br>Elementos da lista: ${lista}`;  
+        mostra_resultado.innerHTML += `<br>Maior elemento da lista: ${Math.min(...lista)}`;
+        mostra_resultado.innerHTML += `<br>Menor elemento da lista: ${Math.min(...lista)}`;
+    }
 }
