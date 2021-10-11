882f78ea5a745772923af90794109739ce15295a
João Victor Campos Souza Oliveira <voliveira.joao30@sempreceub.com>
2021-10-11 10:29:53 -0300

    resolvendo bugs

--- exercicios/exercicio_tabuada/main.js
+++ exercicios/exercicio_tabuada/main.js
@@ -8,24 +8,9 @@ function calcular(){
     } else{
         select_resultado.innerText = "";
         for(let constante = 0; constante <= 10; constante++){
-            let resultado = document.createElement('option');
+            resultado.addEventListener("click", calculo(constante, numero));
             resultado.text = `${constante} x ${numero} = ${constante*numero}`;
             select_resultado.appendChild(resultado);
-            //resultado.addEventListener("click",calculo(constante, numero));
         }
     }
 }
-
-/*function calculo(n1,n2){
-    let calculo = [];
-    let multiplicacao = n1*n2;
-    while(n1 <= multiplicacao){
-        if(n1 === n1 || soma === multiplicacao){
-            calculo.push(n1);
-        } else { 
-            calculo.push(` + ${n1} + `);
-        }
-        n1 =+ n1;
-    } 
-    return mostra_calculo.innerText = calculo;
-}*/
