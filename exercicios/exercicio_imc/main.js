ad239abf4452dbcd33cab06cd6c0c76e6d925dbb
João Victor Campos Souza Oliveira <voliveira.joao30@sempreceub.com>
2021-10-17 00:02:16 -0300

    adição de linhas na tabela, na parte do html, e remoção do posivel bug de aceitar altura e peso negativos, na parte do js.

--- exercicios/exercicio_imc/main.js
+++ exercicios/exercicio_imc/main.js
@@ -5,8 +5,8 @@ function calcular() {
         input_peso = document.getElementById("input_peso"),
         p_imc = document.getElementById("p_imc"),
 
-        altura = Number(input_altura.value).toFixed(2),
-        peso = Number(input_peso.value).toFixed(2),
+        altura = Number(input_altura.value),
+        peso = Number(input_peso.value),
         calculo = peso / (altura * altura);
     
     if(input_altura.value.length === 0 || input_peso.value.length === 0){
@@ -15,23 +15,28 @@ function calcular() {
     else{
         p_imc.innerHTML = "Seu IMC é: ";
 
-        if (calculo < 18.5) {
-            p_imc.innerHTML += `${calculo.toFixed(2)} Você está abaixo do peso`;
+        if(altura <= 0 || peso <= 0){
+            alert("Por favor adicione somente valores maiores que 0");
         }
-        else if (calculo >= 18.5 && calculo <= 24.5) {
-            p_imc.innerHTML += `${calculo.toFixed(2)} Você tem o peso normal`;
-        }
-        else if (calculo >= 25 && calculo <= 29.9) {
-            p_imc.innerHTML += `${calculo.toFixed(2)} Você ta com sobrepeso`;
-        }
-        else if (calculo >= 30 && calculo <= 34.9) {
-            p_imc.innerHTML += `${calculo.toFixed(2)} Você tem Obesidade Grau I`;
-        }
-        else if (calculo >= 35 && calculo <= 39.9) {
-            p_imc.innerHTML += `${calculo.toFixed(2)} Você tem Obesidade Grau II`;
-        }
-        else {
-            p_imc.innerHTML += `${calculo.toFixed(2)} Você tem Obesidade Grau III`;
+        else{
+            if(calculo > 0 && calculo < 18.5){
+                p_imc.innerHTML += `${calculo.toFixed(2)} Você está abaixo do peso`;
+            }
+            else if(calculo >= 18.5 && calculo <= 24.5){
+                p_imc.innerHTML += `${calculo.toFixed(2)} Você tem o peso normal`;
+            }
+            else if(calculo >= 25 && calculo <= 29.9){
+                p_imc.innerHTML += `${calculo.toFixed(2)} Você ta com sobrepeso`;
+            }
+            else if(calculo >= 30 && calculo <= 34.9){
+                p_imc.innerHTML += `${calculo.toFixed(2)} Você tem Obesidade Grau I`;
+            }
+            else if(calculo >= 35 && calculo <= 40){
+                p_imc.innerHTML += `${calculo.toFixed(2)} Você tem Obesidade Grau II`;
+            }
+            else{
+                p_imc.innerHTML += `${calculo.toFixed(2)} Você tem Obesidade Grau III`;
+            }
         }
     }
 }
