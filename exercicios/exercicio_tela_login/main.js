f56e380a11eacc2c13ad011b7fa071d7d4c2b0f5
João Victor Campos Souza Oliveira <voliveira.joao30@sempreceub.com>
2021-10-25 23:33:15 -0300

    atualizações no js e no html, mais a criação do oi.html para um teste.

--- exercicios/exercicio_tela_login/main.js
+++ exercicios/exercicio_tela_login/main.js
@@ -1,13 +1,35 @@
-class conta{
+class Conta{
     constructor(){
         this.id = 0;
+        this.lista = [];
     }
     
-    criarConta(){
-        alert('foi');
+    login(){
+        if(this.recebeDados()){
+            window.location.href = "oi.html";
+        }
+    }
+
+    recebeDados(){
+        let email = document.getElementById("email");
+        let senha = document.getElementById("senha");
+        let mensagem = '';
+
+        if(email.value.length === 0 ){
+            mensagem += 'Por favor preencha o campo de email\n';
+        }
+
+        if(senha.value.length === 0){
+            mensagem += 'Por favor preencha o campo de senha\n';   
+        }
+
+        if(mensagem != ''){
+            alert(mensagem);
+            return false;
+        }
+
+        return true;
     }
 }
 
-let teste = new conta;
-let botao = document.getElementById("botao");
-botao.addEventListener("click", teste.criarConta());
+let teste = new Conta();
